6bd00bca82195c4cc46543c920c37c7d0cd169ee
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-05-21 01:18:52 +0200

    adding JMS listener

---
+++ src/main/java/guru/sfg/common/events/BeerEvent.java
@@ -0,0 +1,18 @@
+package guru.sfg.common.events;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BeerEvent implements Serializable {
+
+    static final long serialVersionUID = 4420602766144722137L;
+    private BeerDto beerDto;
+}
