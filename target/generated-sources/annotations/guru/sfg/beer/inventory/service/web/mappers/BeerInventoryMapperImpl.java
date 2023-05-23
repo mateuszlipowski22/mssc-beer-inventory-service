2db3a3507aabb8f473ba090f5739223a23659e56
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-05-24 01:40:03 +0200

    update artemis config

--- target/generated-sources/annotations/guru/sfg/beer/inventory/service/web/mappers/BeerInventoryMapperImpl.java
+++ target/generated-sources/annotations/guru/sfg/beer/inventory/service/web/mappers/BeerInventoryMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-05-20T00:23:01+0200",
+    date = "2023-05-24T01:28:06+0200",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 20 (Oracle Corporation)"
 )
 @Component
